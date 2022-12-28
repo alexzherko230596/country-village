f202d6ac47f06f71ad151ba12c187cfadd8c9d5d
Oleg Karahin <olegkarahin@MacBook-Air-Oleg.local>
2022-12-28 16:32:59 +0100

    block about

--- src/screen/HomeScreen/HomeScreen.jsx
+++ src/screen/HomeScreen/HomeScreen.jsx
@@ -13,36 +13,29 @@ export const HomeScreen = () => {
                             <h1 className={classes.home__main__wrapper_h1}>Проведите выходные на берегу живописного озера в деревне Янушки </h1>
                             <h2 className={classes.home__main__wrapper_h2}>Проведите выходные на берегу живописного озера в деревне Янушки </h2>
                             <button className={classes.home__main__wrapper_button}>Забронировать</button>
-                        </div>
+                        </div >
                    </div>
                 </section>
                 <section className={classes.home__about}>
-                    <div className="g-container">
-                        <div className={classes.home__about__block}>
-                                <div className={classes.home__about__block__p1}> 
-                                    <div className={classes.home__about__block__p1__box}>
-                                        <p>Комфорт</p>
-                                        <p className={classes.home__about__block__p5}>Мы оборудовали домики всем неообходимым,чтобы вам было удобно</p>
-                                    </div>
-                                </div>
-                            <div className={classes.home__about__block__p2}> 
-                            <div className={classes.home__about__block__p2__box2}>
-                                         <p>Комфорт</p>
-                                        <p className={classes.home__about__block__p6}>Мы оборудовали домики всем неообходимым,чтобы вам было удобно </p>
-                                     </div>
-                             </div>
-                            <div className={classes.home__about__block__p3}>
-                               <div className={classes.home__about__block__p3__box3}>
-                                     <p>Комфорт</p>
-                                     <p className={classes.home__about__block__p7}>Мы оборудовали домики всем неообходимым,чтобы вам было удобно </p>
-                                </div> 
+                    <div className="g-container">  
+                        <div className={classes.home__about__box}> 
+                            <div className={classes.home__about__box__item}>
+                                <p className={classes.home__about__box__item__p}> комфорт</p>  
+                                <p className={classes.home__about__box__item__p1}> Мы оборудовали домики всем необходимым, чтобы вам было удобно </p>
+                            </div> 
+                        
+                            <div className={classes.home__about__box__item}>
+                                <p className={classes.home__about__box__item__p}> комфорт</p>  
+                                <p className={classes.home__about__box__item__p1}> Мы оборудовали домики всем необходимым, чтобы вам было удобно </p>
                             </div>
-                            <div className={classes.home__about__block__p4} >
-                                <div className={classes.home__about__block__p4__box4}>
-                                     <p>Комфорт</p>
-                                     <p className={classes.home__about__block__p8}>Мы оборудовали домики всем неообходимым,чтобы вам было удобно</p>
-                             </div>
-                             </div>
+                            <div className={classes.home__about__box__item}>
+                                <p className={classes.home__about__box__item__p}> комфорт</p>  
+                                <p className={classes.home__about__box__item__p1}> Мы оборудовали домики всем необходимым, чтобы вам было удобно </p>
+                            </div>
+                            <div className={classes.home__about__box__item}>
+                                <p className={classes.home__about__box__item__p}> комфорт</p>  
+                                <p className={classes.home__about__box__item__p1}> Мы оборудовали домики всем необходимым, чтобы вам было удобно </p>
+                            </div>
                         </div>
                     </div>
                 </section>
