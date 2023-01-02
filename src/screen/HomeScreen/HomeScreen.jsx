1ca15d0fbc360c878364fa95ac8c754da2989c4c
Oleg Karahin <olegkarahin@MacBook-Air-Oleg.local>
2023-01-02 13:05:26 +0100

    added block about

--- src/screen/HomeScreen/HomeScreen.jsx
+++ src/screen/HomeScreen/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from './HomeScreen.module.scss'
 import { Header } from "../../components/Header/Header";
+import sauna from '../../img/sauna.svg'
 
 export const HomeScreen = () => {
     return(
@@ -38,6 +39,49 @@ export const HomeScreen = () => {
                         </div>
                     </div>
                 </section>
+                <section className={classes.home__abouthome}>
+                    <div className="g-container" >
+                     <div className={classes.home__abouthome__wrapper}>
+                        <div className={classes.home__abouthome__wrapper__img}> 
+
+                        </div>
+                        <div className={classes.home__abouthome__wrapper__block}>
+                            <p className={classes.home__abouthome__wrapper__block_title}>О Доме</p>
+                           
+                            <div className={classes.home__abouthome__wrapper__block__test} >
+                               <div className={classes.home__abouthome__wrapper__block__test__dscr} > 
+                                   <div className={classes.home__abouthome__wrapper__block__test__dscr__box}> 
+                                        <img src={sauna} className={classes.home__abouthome__wrapper__block__test__dscr__box_img} alt='sauna'/>
+                                        <p>Мы оборудовали домики </p>
+                                    </div> 
+                                    <div className={classes.home__abouthome__wrapper__block__test__dscr__box2}> 
+                                    <img src={sauna} className={classes.home__abouthome__wrapper__block__test__dscr__box_img2} alt='sauna'/>
+                                        <p>Мы оборудовали домики </p>
+                                    </div> 
+                                    
+                               </div>
+                               <div className={classes.home__abouthome__wrapper__block__test__dscr} > 
+                                   <div className={classes.home__abouthome__wrapper__block__test__dscr__box}> 
+                                   <img src={sauna} className={classes.home__abouthome__wrapper__block__test__dscr__box_img} alt='sauna'/>
+                                        <p>Мы оборудовали домики </p>
+                                    </div> 
+                                    <div className={classes.home__abouthome__wrapper__block__test__dscr__box2}> 
+                                    <img src={sauna} className={classes.home__abouthome__wrapper__block__test__dscr__box_img} alt='sauna'/>
+                                        <p>Мы оборудовали домики </p>
+                                    </div> 
+                               </div>
+                               
+                            </div>
+                           
+                        <p className={classes.home__abouthome__wrapper__block_price}>200 рублей/сутки</p>
+                            
+                        </div>
+                        
+                            
+                     </div>
+                 </div>
+
+                 </section>
                
             </div>
         </div>
